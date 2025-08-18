8c7e2bc9cc44dc189937612af361045bf37381be
MWMW1133 <134836235+MWMW1133@users.noreply.github.com>
2025-08-18 15:08:24 +0900

    테이블 수정 및 코드 수정 필

--- src/main/java/com/share/dairy/controller/MoodGraphController.java
+++ src/main/java/com/share/dairy/controller/MoodGraphController.java
@@ -18,6 +18,7 @@ import java.util.ResourceBundle;
 import com.share.dairy.repo.MoodRepository;
 import com.share.dairy.model.mood.MoodPoint;
 
+//diary analysis 테이블 user_id 추가 및 user_id로 mood 조회
 public class MoodGraphController implements Initializable {
 
     @FXML private ToggleButton weekToggle;
